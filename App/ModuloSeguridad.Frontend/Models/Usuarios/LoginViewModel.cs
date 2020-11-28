39059b0731ae59f530e457088ad3cacead6be269
gabriel <gabriel@DESKTOP-2HDF9ON>
2020-11-27 22:35:19 -0300

    organizacion

---
+++ App/ModuloSeguridad.Frontend/Models/Usuarios/LoginViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModuloSeguridad.Frontend.Models.Usuarios
+{
+    public class LoginViewModel
+    {
+    }
+}
